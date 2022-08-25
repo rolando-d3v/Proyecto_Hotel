a876e7a5f41d8af1e218ba87aed44e4e4d3618d1
unknown <landon3388@gmail.com>
2022-08-25 15:49:09 -0500

    re

---
+++ app/src/main/java/com/example/proyecto_hotel/InfoReserva.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_reserva);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_hotel/Portada.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Portada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+    }
+}
